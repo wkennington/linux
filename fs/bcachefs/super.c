4b03c7f102f517cb432ff6c7158867bf90f6af8e
Surbhi Palande <sap@daterainc.com>
2014-08-26 15:23:37 -0700

    Set running bit for a cacheset before you attach a backing device.
    If you attach backing devices before the cache devices then you
    never have a running cache set while attaching the backing device.
    In this case, bch_run_cache_set would always fail to attach the backing
    device as RUNNING_CACHE_SET bit is set after the call to
    bch_cached_dev_attach() rather than before. Set this bit before the
    call.
    
    Signed-off-by: Surbhi Palande <sap@daterainc.com>
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/super.c
+++ fs/bcachefs/super.c
@@ -837,10 +837,8 @@ int bch_cached_dev_attach(struct cached_dev *dc, struct cache_set *c)
 		return -EINVAL;
 	}
 
-	if (!test_bit(CACHE_SET_RUNNING, &c->flags)) {
-		pr_err("Can't attach %s: not running", buf);
-		return -EINVAL;
-	}
+	if (!test_bit(CACHE_SET_RUNNING, &c->flags))
+		return 0;
 
 	if (test_bit(CACHE_SET_STOPPING, &c->flags)) {
 		pr_err("Can't attach %s: shutting down", buf);
@@ -1663,9 +1661,6 @@ static const char *run_cache_set(struct cache_set *c)
 	c->sb.last_mount = get_seconds();
 	bcache_write_super(c);
 
-	list_for_each_entry_safe(dc, t, &uncached_devices, list)
-		bch_cached_dev_attach(dc, c);
-
 	flash_devs_run(c);
 
 	bch_debug_init_cache_set(c);
@@ -1675,6 +1670,8 @@ static const char *run_cache_set(struct cache_set *c)
 		goto err;
 
 	set_bit(CACHE_SET_RUNNING, &c->flags);
+	list_for_each_entry_safe(dc, t, &uncached_devices, list)
+		bch_cached_dev_attach(dc, c);
 
 	closure_put(&c->caching);
 
