a5bd0a2f049c6647b55e4c8ba5f0bdadca131254
Slava Pestov <sviatoslavpestov@gmail.com>
2014-11-24 22:17:16 -0800

    bcachefs: tiering now does strict round robin between devices
    This should keep each disk's queue full of sequential writes at
    all times, leading to improved performance when tiering many
    small extents.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/sysfs.c
+++ fs/bcachefs/sysfs.c
@@ -154,9 +154,11 @@ rw_attribute(copy_gc_enabled);
 sysfs_queue_attribute(copy_gc);
 sysfs_pd_controller_attribute(copy_gc);
 rw_attribute(tiering_enabled);
-sysfs_queue_attribute(tiering);
 rw_attribute(tiering_percent);
 sysfs_pd_controller_attribute(tiering);
+
+sysfs_queue_attribute(tiering);
+rw_attribute(tiering_stripe_size);
 
 sysfs_pd_controller_attribute(foreground_write);
 
@@ -601,10 +603,8 @@ SHOW(__bch_cache_set)
 	sysfs_print(sector_reserve_percent,	c->sector_reserve_percent);
 
 	sysfs_printf(tiering_enabled,		"%i", c->tiering_enabled);
-	sysfs_queue_show(tiering,		&c->tiering_queue);
 	sysfs_print(tiering_percent,		c->tiering_percent);
 	sysfs_pd_controller_show(tiering,	&c->tiering_pd);
-
 
 	sysfs_print(btree_flush_delay,		c->btree_flush_delay);
 
@@ -768,8 +768,6 @@ STORE(__bch_cache_set)
 
 	sysfs_strtoul(tiering_percent,		c->tiering_percent);
 	sysfs_pd_controller_store(tiering,	&c->tiering_pd);
-	sysfs_queue_store(tiering,		&c->tiering_queue);
-
 
 	if (attr == &sysfs_add_device) {
 		char *path = kstrdup(buf, GFP_KERNEL);
@@ -911,7 +909,6 @@ static struct attribute *bch_cache_set_internal_files[] = {
 	&sysfs_copy_gc_enabled,
 	&sysfs_tiering_enabled,
 	sysfs_pd_controller_files(tiering),
-	sysfs_queue_files(tiering),
 	sysfs_pd_controller_files(foreground_write),
 
 	NULL
@@ -1031,6 +1028,9 @@ SHOW(__bch_cache)
 	sysfs_pd_controller_show(copy_gc, &ca->moving_gc_pd);
 	sysfs_queue_show(copy_gc, &ca->moving_gc_queue);
 
+	sysfs_queue_show(tiering, &ca->tiering_queue);
+	sysfs_print(tiering_stripe_size, ca->tiering_stripe_size);
+
 	if (attr == &sysfs_cache_replacement_policy)
 		return bch_snprint_string_list(buf, PAGE_SIZE,
 					       cache_replacement_policies,
@@ -1064,6 +1064,9 @@ STORE(__bch_cache)
 
 	sysfs_pd_controller_store(copy_gc, &ca->moving_gc_pd);
 	sysfs_queue_store(copy_gc, &ca->moving_gc_queue);
+
+	sysfs_queue_store(tiering, &ca->tiering_queue);
+	sysfs_strtoul(tiering_stripe_size, ca->tiering_stripe_size);
 
 	if (attr == &sysfs_discard) {
 		bool v = strtoul_or_return(buf);
@@ -1200,6 +1203,8 @@ static struct attribute *bch_cache_files[] = {
 	&sysfs_state_rw,
 	sysfs_pd_controller_files(copy_gc),
 	sysfs_queue_files(copy_gc),
+	sysfs_queue_files(tiering),
+	&sysfs_tiering_stripe_size,
 	NULL
 };
 KTYPE(bch_cache);
