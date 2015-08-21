d90396f5fa7c2ed151d9689ad062668a048c8b29
Kent Overstreet <kent.overstreet@gmail.com>
2015-08-21 00:33:08 -0800

    bcachefs: rip out list of open buckets for a simple hash by write point
    Also add an explicit struct write_point, that the caller of bch_data_insert()
    specifies: the idea is that write_point specifies how the allocation should
    happen.
    
    This makes a lot of things cleaner - copygc can specify the correct write point
    before calling bch_data_move(), which means that it can strip off the pointer to
    the old copy instead having gc_alloc_sectors() do everything - the old way with
    gc_alloc_sectors() was fairly sketchy and difficult to follow.
    
    Also, since bch_alloc_sectors() doesn't modify any of the old pointers we can
    get rid of the old ptrs_to_write() bitfield - bch_alloc_sectors is only
    appending new pointers, so we just write pointers starting from wherever they
    ended when we started.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/btree.c
+++ fs/bcachefs/btree.c
@@ -451,7 +451,6 @@ static void do_btree_node_write(struct btree *b)
 {
 	struct closure *cl = &b->io;
 	struct bset *i = btree_bset_last(b);
-	unsigned long ptrs_to_write[BITS_TO_LONGS(MAX_CACHES_PER_SET)];
 	BKEY_PADDED(key) k;
 	int n;
 
@@ -472,8 +471,6 @@ static void do_btree_node_write(struct btree *b)
 	b->bio->bi_iter.bi_size	= roundup(set_bytes(i), block_bytes(b->c));
 	b->bio->bi_opf		= REQ_OP_WRITE | REQ_META | REQ_FUA;
 	bch_bio_map(b->bio, i);
-
-	memset(ptrs_to_write, 0xFF, sizeof(ptrs_to_write));
 
 	/*
 	 * If we're appending to a leaf node, we don't technically need FUA -
@@ -504,8 +501,7 @@ static void do_btree_node_write(struct btree *b)
 			memcpy(page_address(bv->bv_page),
 			       base + j * PAGE_SIZE, PAGE_SIZE);
 
-		bch_submit_bbio_replicas(b->bio, b->c, &k.key,
-					 ptrs_to_write, true);
+		bch_submit_bbio_replicas(b->bio, b->c, &k.key, 0, true);
 		continue_at(cl, btree_node_write_done, NULL);
 	} else {
 		trace_bcache_btree_bounce_write_fail(b);
@@ -513,8 +509,7 @@ static void do_btree_node_write(struct btree *b)
 		b->bio->bi_vcnt = 0;
 		bch_bio_map(b->bio, i);
 
-		bch_submit_bbio_replicas(b->bio, b->c, &k.key,
-					 ptrs_to_write, true);
+		bch_submit_bbio_replicas(b->bio, b->c, &k.key, 0, true);
 
 		closure_sync(cl);
 		continue_at_nobarrier(cl, __btree_node_write_done, NULL);
