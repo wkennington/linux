81eb0700807f342f34bc027656fec901f797487a
Kent Overstreet <kent.overstreet@gmail.com>
2015-08-20 23:46:08 -0800

    *** bcache: fix reserve/freelist calculations (again)
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/super.c
+++ fs/bcachefs/super.c
@@ -146,7 +146,7 @@ static const char *read_super(struct cache_sb *sb, struct block_device *bdev,
 			goto err;
 
 		err = "Not enough buckets";
-		if (sb->nbuckets < 1 << 7)
+		if (sb->nbuckets < 1 << 8)
 			goto err;
 
 		err = "Bad block/bucket size";
@@ -1829,10 +1829,10 @@ static int cache_alloc(struct cache *ca)
 
 	bio_init(&ca->journal.bio, ca->journal.bio.bi_inline_vecs, 8);
 
-	free = roundup_pow_of_two(ca->sb.nbuckets) >> 10;
+	free = max_t(size_t, 4, ca->sb.nbuckets >> 10);
 
 	if (!init_fifo(&ca->free[RESERVE_BTREE], 8, GFP_KERNEL) ||
-	    !init_fifo_exact(&ca->free[RESERVE_PRIO], prio_buckets(ca), GFP_KERNEL) ||
+	    !init_fifo(&ca->free[RESERVE_PRIO], prio_buckets(ca), GFP_KERNEL) ||
 	    !init_fifo(&ca->free[RESERVE_MOVINGGC], free, GFP_KERNEL) ||
 	    !init_fifo(&ca->free[RESERVE_NONE], free, GFP_KERNEL) ||
 	    !init_fifo(&ca->free_inc,	free << 2, GFP_KERNEL) ||
