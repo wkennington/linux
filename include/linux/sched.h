1e04be8ca1e4171dc0b704ab591301bf27f3f2be
Kent Overstreet <kent.overstreet@gmail.com>
2017-03-16 22:18:50 -0800

    bcachefs: Initial commit
    Fork of drivers/md/bcache

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1024,7 +1024,7 @@ struct task_struct {
 #ifdef CONFIG_UPROBES
 	struct uprobe_task		*utask;
 #endif
-#if defined(CONFIG_BCACHE) || defined(CONFIG_BCACHE_MODULE)
+#if defined(CONFIG_BCACHE) || defined(CONFIG_BCACHE_MODULE) || defined(CONFIG_BCACHEFS_FS) || defined(CONFIG_BCACHEFS_FS_MODULE)
 	unsigned int			sequential_io;
 	unsigned int			sequential_io_avg;
 #endif
