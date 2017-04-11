23667950b210105f954d508ef46e555ced6e34d3
Kent Overstreet <kent.overstreet@gmail.com>
2017-04-10 23:44:11 -0800

    bcachefs: Kill bucket_mark.copygc

--- fs/bcachefs/super.c
+++ fs/bcachefs/super.c
@@ -984,7 +984,8 @@ static void bch2_dev_free(struct bch_dev *ca)
 	kfree(ca->bio_prio);
 	kvpfree(ca->buckets,	 ca->mi.nbuckets * sizeof(struct bucket));
 	kvpfree(ca->oldest_gens, ca->mi.nbuckets * sizeof(u8));
-	free_heap(&ca->heap);
+	free_heap(&ca->copygc_heap);
+	free_heap(&ca->alloc_heap);
 	free_fifo(&ca->free_inc);
 
 	for (i = 0; i < RESERVE_NR; i++)
@@ -1105,7 +1106,6 @@ static int bch2_dev_alloc(struct bch_fs *c, unsigned dev_idx)
 
 	spin_lock_init(&ca->freelist_lock);
 	spin_lock_init(&ca->prio_buckets_lock);
-	mutex_init(&ca->heap_lock);
 	mutex_init(&ca->prio_write_lock);
 	bch2_dev_moving_gc_init(ca);
 
@@ -1142,7 +1142,8 @@ static int bch2_dev_alloc(struct bch_fs *c, unsigned dev_idx)
 		       movinggc_reserve, GFP_KERNEL) ||
 	    !init_fifo(&ca->free[RESERVE_NONE], reserve_none, GFP_KERNEL) ||
 	    !init_fifo(&ca->free_inc,	free_inc_reserve, GFP_KERNEL) ||
-	    !init_heap(&ca->heap,	heap_size, GFP_KERNEL) ||
+	    !init_heap(&ca->alloc_heap,	heap_size, GFP_KERNEL) ||
+	    !init_heap(&ca->copygc_heap,heap_size, GFP_KERNEL) ||
 	    !(ca->oldest_gens	= kvpmalloc(ca->mi.nbuckets *
 					    sizeof(u8),
 					    GFP_KERNEL|__GFP_ZERO)) ||
