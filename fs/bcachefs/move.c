koverstreet/bcachefs

bcachefs: Copy data to slower tier in background
Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

fs/bcachefs/move.c
@@ -1,0 +1,101 @@
+
+#include "bcache.h"
+#include "btree.h"
+#include "extents.h"
+#include "keybuf.h"
+#include "move.h"
+
+#include <trace/events/bcachefs.h>
+
+static void moving_init(struct moving_io *io)
+{
+	struct bio *bio = &io->bio.bio;
+
+	bio_init(bio);
+	bio_get(bio);
+	bio_set_prio(bio, IOPRIO_PRIO_VALUE(IOPRIO_CLASS_IDLE, 0));
+
+	bio->bi_iter.bi_size	= KEY_SIZE(&io->w->key) << 9;
+	bio->bi_max_vecs	= DIV_ROUND_UP(KEY_SIZE(&io->w->key),
+					       PAGE_SECTORS);
+	bio->bi_private		= &io->cl;
+	bio->bi_io_vec		= bio->bi_inline_vecs;
+	bch_bio_map(bio, NULL);
+}
+
+static void moving_io_destructor(struct closure *cl)
+{
+	struct moving_io *io = container_of(cl, struct moving_io, cl);
+
+	bch_keybuf_del(io->keybuf, io->w);
+	kfree(io);
+}
+
+static void write_moving_finish(struct closure *cl)
+{
+	struct moving_io *io = container_of(cl, struct moving_io, cl);
+
+	bio_free_pages(&io->bio.bio);
+
+	if (io->op.replace_collision)
+		trace_bcache_gc_copy_collision(&io->w->key);
+
+	closure_return_with_destructor(cl, moving_io_destructor);
+}
+
+static void write_moving(struct closure *cl)
+{
+	struct moving_io *io = container_of(cl, struct moving_io, cl);
+	struct data_insert_op *op = &io->op;
+
+	if (!op->error)	{
+		moving_init(io);
+
+		op->bio->bi_iter.bi_sector = KEY_START(&io->w->key);
+
+		closure_call(&op->cl, bch_data_insert, NULL, cl);
+	}
+
+	continue_at(cl, write_moving_finish, io->op.wq);
+}
+
+static void read_moving_endio(struct bio *bio)
+{
+	struct bbio *b = container_of(bio, struct bbio, bio);
+	struct moving_io *io = container_of(bio->bi_private,
+					    struct moving_io, cl);
+
+	if (bio->bi_error)
+		io->op.error = bio->bi_error;
+	else if (!KEY_DIRTY(&b->key) &&
+		 ptr_stale(io->op.c, &b->key, 0)) {
+		io->op.error = -EINTR;
+	}
+
+	bch_bbio_endio(io->op.c, bio, bio->bi_error, "reading data to move");
+}
+
+void bch_data_move(struct closure *cl)
+{
+	struct moving_io *io = container_of(cl, struct moving_io, cl);
+	struct bio *bio = &io->bio.bio;
+	int ptr;
+
+	/* bail out if all pointers are stale */
+	ptr = bch_extent_pick_ptr(io->op.c, &io->w->key);
+	if (ptr < 0)
+		closure_return_with_destructor(cl, moving_io_destructor);
+
+	moving_init(io);
+	bio = &io->bio.bio;
+
+	if (bio_alloc_pages(bio, GFP_KERNEL))
+		closure_return_with_destructor(cl, moving_io_destructor);
+
+	bio_set_op_attrs(bio, REQ_OP_READ, 0);
+	bio->bi_end_io	= read_moving_endio;
+
+	bch_submit_bbio(bio, io->op.c, &io->w->key, ptr);
+
+	continue_at(cl, write_moving, io->op.wq);
+}

fs/bcachefs/tier.c
@@ -1,0 +1,155 @@
+
+#include "bcache.h"
+#include "btree.h"
+#include "extents.h"
+#include "keybuf.h"
+#include "move.h"
+
+#include <linux/delay.h>
+#include <linux/freezer.h>
+#include <linux/kthread.h>
+#include <trace/events/bcachefs.h>
+
+#define TIERING_THRESHOLD	100
+
+static void __update_tiering_rate(struct cache_set *c)
+{
+	unsigned i, j;
+	u64 tier_dirty[CACHE_TIERS];
+	u64 tier_size[CACHE_TIERS];
+
+	for (i = 0; i < CACHE_TIERS; i++) {
+		struct cache_tier *tier = c->cache_by_alloc + i;
+
+		for (j = 0; j < tier->nr_devices; j++) {
+			struct cache *ca = tier->devices[j];
+
+			tier_size[i] += ca->sb.nbuckets - ca->sb.first_bucket;
+			tier_dirty[i] += ca->sb.nbuckets - ca->sb.first_bucket -
+				ca->buckets_free;
+		}
+	}
+
+	if (tier_size[1]) {
+		u64 target = div64_u64(tier_size[0] * tier_dirty[1],
+				       tier_size[1]);
+
+		bch_pd_controller_update(&c->tiering_pd,
+					 target << (c->bucket_bits + 9),
+					 tier_dirty[0] << (c->bucket_bits + 9));
+	}
+}
+
+static void update_tiering_rate(struct work_struct *work)
+{
+	struct cache_set *c = container_of(to_delayed_work(work),
+					   struct cache_set,
+					   tiering_pd.update);
+	__update_tiering_rate(c);
+
+	schedule_delayed_work(&c->tiering_pd.update,
+			      c->tiering_pd.update_seconds * HZ);
+}
+
+static bool tiering_pred(struct keybuf *buf, struct bkey *k)
+{
+	struct cache_set *c = container_of(buf, struct cache_set, tiering_keys);
+
+	return KEY_PTRS(k) &&
+		ptr_available(c, k, KEY_PTRS(k) - 1) &&
+		!PTR_TIER(c, k, KEY_PTRS(k) - 1);
+}
+
+static void read_tiering(struct cache_set *c)
+{
+	struct keybuf_key *w;
+	struct moving_io *io;
+	struct closure cl;
+
+	closure_init_stack(&cl);
+
+	/* XXX: if we error, background writeback could stall indefinitely */
+
+	c->tiering_keys.last_scanned = ZERO_KEY;
+
+	while (!bch_ratelimit_wait_freezable_stoppable(&c->tiering_pd.rate,
+						       &cl)) {
+		w = bch_keybuf_next_rescan(c, &c->tiering_keys,
+					   &MAX_KEY, tiering_pred);
+		if (!w)
+			break;
+
+		if (ptr_stale(c, &w->key, 0)) {
+			bch_keybuf_del(&c->tiering_keys, w);
+			continue;
+		}
+
+		io = kzalloc(sizeof(struct moving_io) + sizeof(struct bio_vec)
+			     * DIV_ROUND_UP(KEY_SIZE(&w->key), PAGE_SECTORS),
+			     GFP_KERNEL);
+		if (!io)
+			goto err;
+
+		w->private	= io;
+		io->w		= w;
+		io->keybuf	= &c->tiering_keys;
+
+		bch_data_insert_op_init(&io->op, c, c->tiering_wq,
+					&io->bio.bio, 0,
+					false, false, false,
+					&io->w->key, &io->w->key);
+
+		io->op.tier	= PTR_TIER(c, &w->key,
+					   KEY_PTRS(&w->key) - 1) + 1;
+
+		bch_ratelimit_increment(&c->tiering_pd.rate,
+					KEY_SIZE(&w->key) << 9);
+
+		closure_call(&io->cl, bch_data_move, NULL, &cl);
+	}
+
+	if (0) {
+err:		if (!IS_ERR_OR_NULL(w->private))
+			kfree(w->private);
+
+		bch_keybuf_del(&c->tiering_keys, w);
+	}
+
+	closure_sync(&cl);
+}
+
+static int bch_tiering_thread(void *arg)
+{
+	struct cache_set *c = arg;
+	unsigned long last = jiffies;
+
+	do {
+		if (kthread_wait_freezable(c->tiering_enabled &&
+					   c->cache_by_alloc[1].nr_devices))
+			break;
+
+		read_tiering(c);
+	} while (!bch_kthread_loop_ratelimit(&last,
+					     c->btree_scan_ratelimit * HZ));
+
+	return 0;
+}
+
+void bch_tiering_init_cache_set(struct cache_set *c)
+{
+	bch_keybuf_init(&c->tiering_keys);
+	INIT_DELAYED_WORK(&c->tiering_pd.update, update_tiering_rate);
+}
+
+int bch_tiering_thread_start(struct cache_set *c)
+{
+	c->tiering_thread = kthread_create(bch_tiering_thread, c,
+					   "bcache_tier");
+	if (IS_ERR(c->tiering_thread))
+		return PTR_ERR(c->tiering_thread);
+
+	bch_pd_controller_start(&c->tiering_pd);
+	wake_up_process(c->tiering_thread);
+
+	return 0;
+}

wkennington/linux

fs/bcachefs/move.c
@@ -1,0 +1,99 @@
+
+#include "bcachefs.h"
+#include "btree.h"
+#include "extents.h"
+#include "keybuf.h"
+#include "move.h"
+
+#include <trace/events/bcachefs.h>
+
+static void moving_init(struct moving_io *io)
+{
+	struct bio *bio = &io->bio.bio;
+
+	bio_init(bio, bio->bi_inline_vecs,
+		 DIV_ROUND_UP(KEY_SIZE(&io->w->key), PAGE_SECTORS));
+	bio_get(bio);
+	bio_set_prio(bio, IOPRIO_PRIO_VALUE(IOPRIO_CLASS_IDLE, 0));
+
+	bio->bi_iter.bi_size	= KEY_SIZE(&io->w->key) << 9;
+	bio->bi_private		= &io->cl;
+	bch_bio_map(bio, NULL);
+}
+
+static void moving_io_destructor(struct closure *cl)
+{
+	struct moving_io *io = container_of(cl, struct moving_io, cl);
+
+	bch_keybuf_del(io->keybuf, io->w);
+	kfree(io);
+}
+
+static void write_moving_finish(struct closure *cl)
+{
+	struct moving_io *io = container_of(cl, struct moving_io, cl);
+
+	bio_free_pages(&io->bio.bio);
+
+	if (io->op.replace_collision)
+		trace_bcache_gc_copy_collision(&io->w->key);
+
+	closure_return_with_destructor(cl, moving_io_destructor);
+}
+
+static void write_moving(struct closure *cl)
+{
+	struct moving_io *io = container_of(cl, struct moving_io, cl);
+	struct data_insert_op *op = &io->op;
+
+	if (!op->error)	{
+		moving_init(io);
+
+		op->bio->bi_iter.bi_sector = KEY_START(&io->w->key);
+
+		closure_call(&op->cl, bch_data_insert, NULL, cl);
+	}
+
+	continue_at(cl, write_moving_finish, io->op.wq);
+}
+
+static void read_moving_endio(struct bio *bio)
+{
+	struct bbio *b = container_of(bio, struct bbio, bio);
+	struct moving_io *io = container_of(bio->bi_private,
+					    struct moving_io, cl);
+
+	if (bio->bi_error)
+		io->op.error = bio->bi_error;
+	else if (!KEY_DIRTY(&b->key) &&
+		 ptr_stale(io->op.c, &b->key, 0)) {
+		io->op.error = -EINTR;
+	}
+
+	bch_bbio_endio(io->op.c, bio, bio->bi_error, "reading data to move");
+}
+
+void bch_data_move(struct closure *cl)
+{
+	struct moving_io *io = container_of(cl, struct moving_io, cl);
+	struct bio *bio = &io->bio.bio;
+	int ptr;
+
+	/* bail out if all pointers are stale */
+	ptr = bch_extent_pick_ptr(io->op.c, &io->w->key);
+	if (ptr < 0)
+		closure_return_with_destructor(cl, moving_io_destructor);
+
+	moving_init(io);
+	bio = &io->bio.bio;
+
+	if (bio_alloc_pages(bio, GFP_KERNEL))
+		closure_return_with_destructor(cl, moving_io_destructor);
+
+	bio_set_op_attrs(bio, REQ_OP_READ, 0);
+	bio->bi_end_io	= read_moving_endio;
+
+	bch_submit_bbio(bio, io->op.c, &io->w->key, ptr);
+
+	continue_at(cl, write_moving, io->op.wq);
+}

fs/bcachefs/tier.c
@@ -1,0 +1,155 @@
+
+#include "bcachefs.h"
+#include "btree.h"
+#include "extents.h"
+#include "keybuf.h"
+#include "move.h"
+
+#include <linux/delay.h>
+#include <linux/freezer.h>
+#include <linux/kthread.h>
+#include <trace/events/bcachefs.h>
+
+#define TIERING_THRESHOLD	100
+
+static void __update_tiering_rate(struct cache_set *c)
+{
+	unsigned i, j;
+	u64 tier_dirty[CACHE_TIERS];
+	u64 tier_size[CACHE_TIERS];
+
+	for (i = 0; i < CACHE_TIERS; i++) {
+		struct cache_tier *tier = c->cache_by_alloc + i;
+
+		for (j = 0; j < tier->nr_devices; j++) {
+			struct cache *ca = tier->devices[j];
+
+			tier_size[i] += ca->sb.nbuckets - ca->sb.first_bucket;
+			tier_dirty[i] += ca->sb.nbuckets - ca->sb.first_bucket -
+				ca->buckets_free;
+		}
+	}
+
+	if (tier_size[1]) {
+		u64 target = div64_u64(tier_size[0] * tier_dirty[1],
+				       tier_size[1]);
+
+		bch_pd_controller_update(&c->tiering_pd,
+					 target << (c->bucket_bits + 9),
+					 tier_dirty[0] << (c->bucket_bits + 9));
+	}
+}
+
+static void update_tiering_rate(struct work_struct *work)
+{
+	struct cache_set *c = container_of(to_delayed_work(work),
+					   struct cache_set,
+					   tiering_pd.update);
+	__update_tiering_rate(c);
+
+	schedule_delayed_work(&c->tiering_pd.update,
+			      c->tiering_pd.update_seconds * HZ);
+}
+
+static bool tiering_pred(struct keybuf *buf, struct bkey *k)
+{
+	struct cache_set *c = container_of(buf, struct cache_set, tiering_keys);
+
+	return KEY_PTRS(k) &&
+		ptr_available(c, k, KEY_PTRS(k) - 1) &&
+		!PTR_TIER(c, k, KEY_PTRS(k) - 1);
+}
+
+static void read_tiering(struct cache_set *c)
+{
+	struct keybuf_key *w;
+	struct moving_io *io;
+	struct closure cl;
+
+	closure_init_stack(&cl);
+
+	/* XXX: if we error, background writeback could stall indefinitely */
+
+	c->tiering_keys.last_scanned = ZERO_KEY;
+
+	while (!bch_ratelimit_wait_freezable_stoppable(&c->tiering_pd.rate,
+						       &cl)) {
+		w = bch_keybuf_next_rescan(c, &c->tiering_keys,
+					   &MAX_KEY, tiering_pred);
+		if (!w)
+			break;
+
+		if (ptr_stale(c, &w->key, 0)) {
+			bch_keybuf_del(&c->tiering_keys, w);
+			continue;
+		}
+
+		io = kzalloc(sizeof(struct moving_io) + sizeof(struct bio_vec)
+			     * DIV_ROUND_UP(KEY_SIZE(&w->key), PAGE_SECTORS),
+			     GFP_KERNEL);
+		if (!io)
+			goto err;
+
+		w->private	= io;
+		io->w		= w;
+		io->keybuf	= &c->tiering_keys;
+
+		bch_data_insert_op_init(&io->op, c, c->tiering_wq,
+					&io->bio.bio, 0,
+					false, false, false,
+					&io->w->key, &io->w->key);
+
+		io->op.tier	= PTR_TIER(c, &w->key,
+					   KEY_PTRS(&w->key) - 1) + 1;
+
+		bch_ratelimit_increment(&c->tiering_pd.rate,
+					KEY_SIZE(&w->key) << 9);
+
+		closure_call(&io->cl, bch_data_move, NULL, &cl);
+	}
+
+	if (0) {
+err:		if (!IS_ERR_OR_NULL(w->private))
+			kfree(w->private);
+
+		bch_keybuf_del(&c->tiering_keys, w);
+	}
+
+	closure_sync(&cl);
+}
+
+static int bch_tiering_thread(void *arg)
+{
+	struct cache_set *c = arg;
+	unsigned long last = jiffies;
+
+	do {
+		if (kthread_wait_freezable(c->tiering_enabled &&
+					   c->cache_by_alloc[1].nr_devices))
+			break;
+
+		read_tiering(c);
+	} while (!bch_kthread_loop_ratelimit(&last,
+					     c->btree_scan_ratelimit * HZ));
+
+	return 0;
+}
+
+void bch_tiering_init_cache_set(struct cache_set *c)
+{
+	bch_keybuf_init(&c->tiering_keys);
+	INIT_DELAYED_WORK(&c->tiering_pd.update, update_tiering_rate);
+}
+
+int bch_tiering_thread_start(struct cache_set *c)
+{
+	c->tiering_thread = kthread_create(bch_tiering_thread, c,
+					   "bcache_tier");
+	if (IS_ERR(c->tiering_thread))
+		return PTR_ERR(c->tiering_thread);
+
+	bch_pd_controller_start(&c->tiering_pd);
+	wake_up_process(c->tiering_thread);
+
+	return 0;
+}