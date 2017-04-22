koverstreet/bcachefs

bcachefs: redo GC marks using compare and exchange
This patch changes the representation of bucket marks to an array of a new
bucket_mark struct. Elements of this array are updated atomically with a
new bucket_cmpxchg() macro. The old GC mark states map roughly as follows:

- GC_MARK_METADATA => mark.is_metadata = 1
- GC_MARK_RECLAIMABLE => mark.dirty_sectors = 0, mark.cached_sectors > 0
- GC_MARK_DIRTY => mark.dirty_sectors > 0, or mark.owned_by_allocator = 1
- GC_SECTOR_COUNT => mark.dirty_sectors, or mark.cached_sectors

Cached and dirty sectors are now counted separately. Also, a new
mark.owned_by_allocator flag is used for buckets that are on free lists,
and open buckets, in place of GC_MARK_DIRTY.

Right now, the owned_by_allocator flag is not cleared until the next
garbage collection. This ensures that open buckets do not become
candidates for invalidation, since they do not have an up to date sector
counts. Once sector counts are kept up to date by foreground writes, the
owned_by_allocator flag can be cleared once the open bucket is put.

Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

fs/bcachefs/buckets.c
@@ -1,0 +1,141 @@
+/*
+ * Code for manipulating bucket marks for garbage collection.
+ *
+ * Copyright 2014 Datera, Inc.
+ *
+ * Bucket states:
+ * - free bucket: mark == 0
+ *   The bucket contains no data and will not be read
+ *
+ * - allocator bucket: owned_by_allocator == 1
+ *   The bucket is on a free list, or it is an open bucket
+ *
+ * - cached bucket: owned_by_allocator == 0 &&
+ *                  dirty_sectors == 0 &&
+ *                  cached_sectors > 0
+ *   The bucket contains data but may be safely discarded as the
+ *   we have another replica of the data on a cache device, or it
+ *   has been written back to the backing device
+ *
+ * - dirty bucket: owned_by_allocator == 0 &&
+ *                 dirty_sectors > 0
+ *   The bucket contains data that we only have one copy of
+ *
+ * - metadata bucket: owned_by_allocator == 0 && is_metadata == 1
+ *   This is a btree node, journal or prio bucket
+ *
+ * Lifecycle:
+ *
+ * bucket invalidated => bucket on freelist => open bucket =>
+ *     dirty bucket => clean bucket => bucket invalidated => ...
+ *
+ * Transitions:
+ *
+ * - free => allocator: bucket was invalidated
+ * - cached => allocator: bucket was invalidated
+ *
+ * - allocator => dirty: open bucket was filled up
+ * - allocator => cached: open bucket was filled up
+ * - allocator => metadata: metadata was allocated
+ *
+ * - dirty => cached: dirty sectors were overwritten
+ * - dirty => free: dirty sectors were overwritten
+ * - cached => free: cached sectors were overwritten
+ *
+ * - metadata => free: metadata was freed
+ *
+ * Oddities:
+ * - cached => dirty: a device was removed so formerly replicated data
+ *                    is no longer sufficiently replicated
+ * - free => cached: cannot happen
+ * - free => dirty: cannot happen
+ * - free => metadata: cannot happen
+ */
+
+#include "bcache.h"
+#include "buckets.h"
+
+#include <trace/events/bcachefs.h>
+
+#define bucket_cmpxchg(g, old, new, expr)			\
+do {								\
+	old = (g)->mark;					\
+	while (1) {						\
+		u32 _v;						\
+								\
+		new.counter = old.counter;			\
+		expr;						\
+		_v = cmpxchg(&(g)->mark.counter,		\
+			     old.counter,			\
+			     new.counter);			\
+		if (old.counter == _v)				\
+			break;					\
+		old.counter = _v;				\
+	}							\
+} while (0)
+
+void bch_mark_free_bucket(struct cache *ca, struct bucket *g)
+{
+	struct bucket_mark old, new;
+
+	bucket_cmpxchg(g, old, new, ({
+		BUG_ON(old.dirty_sectors);
+		BUG_ON(old.cached_sectors);
+		new.counter = 0;
+	}));
+}
+
+void bch_mark_alloc_bucket(struct cache *ca, struct bucket *g)
+{
+	struct bucket_mark old, new;
+
+	bucket_cmpxchg(g, old, new, ({
+		BUG_ON(old.dirty_sectors);
+		new.counter = 0;
+		new.owned_by_allocator = 1;
+	}));
+
+	if (!old.owned_by_allocator && old.cached_sectors)
+		trace_bcache_invalidate(ca, g - ca->buckets,
+					old.cached_sectors);
+}
+
+void bch_mark_metadata_bucket(struct cache *ca, struct bucket *g)
+{
+	struct bucket_mark old, new;
+
+	bucket_cmpxchg(g, old, new, ({
+		BUG_ON(old.cached_sectors);
+		BUG_ON(old.dirty_sectors);
+		new.is_metadata = 1;
+		new.owned_by_allocator = 0;
+	}));
+}
+
+#define saturated_add(ca, dst, src, max)			\
+do {								\
+	if ((dst) == (max))					\
+		;						\
+	else if ((dst) + (src) <= (max))			\
+		dst += (src);					\
+	else {							\
+		dst = (max);					\
+		trace_bcache_sectors_saturated(ca);		\
+	}							\
+} while (0)
+
+void bch_mark_data_bucket(struct cache *ca, struct bucket *g,
+			  int sectors, bool dirty)
+{
+	struct bucket_mark old, new;
+
+	bucket_cmpxchg(g, old, new, ({
+		BUG_ON(old.is_metadata);
+		if (dirty)
+			saturated_add(ca, new.dirty_sectors, sectors,
+				      GC_MAX_SECTORS_USED);
+		else
+			saturated_add(ca, new.cached_sectors, sectors,
+				      GC_MAX_SECTORS_USED);
+	}));
+}

fs/bcachefs/buckets.h
@@ -1,0 +1,32 @@
+/*
+ * Code for manipulating bucket marks for garbage collection.
+ *
+ * Copyright 2014 Datera, Inc.
+ */
+
+#ifndef _BUCKETS_H
+#define _BUCKETS_H
+
+#include "bcache.h"
+
+/* The dirty and cached sector counts saturate. If this occurs,
+ * reference counting alone will not free the bucket, and a btree
+ * GC must be performed. */
+#define GC_MAX_SECTORS_USED ((1U << 15) - 1)
+
+static inline bool bucket_unused(struct bucket *b)
+{
+	return !b->mark.counter;
+}
+
+static inline unsigned bucket_sectors_used(struct bucket *b)
+{
+	return b->mark.dirty_sectors + b->mark.cached_sectors;
+}
+
+void bch_mark_free_bucket(struct cache *, struct bucket *);
+void bch_mark_alloc_bucket(struct cache *, struct bucket *);
+void bch_mark_metadata_bucket(struct cache *, struct bucket *);
+void bch_mark_data_bucket(struct cache *, struct bucket *, int, bool);
+
+#endif

wkennington/linux

fs/bcachefs/buckets.c
@@ -1,0 +1,141 @@
+/*
+ * Code for manipulating bucket marks for garbage collection.
+ *
+ * Copyright 2014 Datera, Inc.
+ *
+ * Bucket states:
+ * - free bucket: mark == 0
+ *   The bucket contains no data and will not be read
+ *
+ * - allocator bucket: owned_by_allocator == 1
+ *   The bucket is on a free list, or it is an open bucket
+ *
+ * - cached bucket: owned_by_allocator == 0 &&
+ *                  dirty_sectors == 0 &&
+ *                  cached_sectors > 0
+ *   The bucket contains data but may be safely discarded as the
+ *   we have another replica of the data on a cache device, or it
+ *   has been written back to the backing device
+ *
+ * - dirty bucket: owned_by_allocator == 0 &&
+ *                 dirty_sectors > 0
+ *   The bucket contains data that we only have one copy of
+ *
+ * - metadata bucket: owned_by_allocator == 0 && is_metadata == 1
+ *   This is a btree node, journal or prio bucket
+ *
+ * Lifecycle:
+ *
+ * bucket invalidated => bucket on freelist => open bucket =>
+ *     dirty bucket => clean bucket => bucket invalidated => ...
+ *
+ * Transitions:
+ *
+ * - free => allocator: bucket was invalidated
+ * - cached => allocator: bucket was invalidated
+ *
+ * - allocator => dirty: open bucket was filled up
+ * - allocator => cached: open bucket was filled up
+ * - allocator => metadata: metadata was allocated
+ *
+ * - dirty => cached: dirty sectors were overwritten
+ * - dirty => free: dirty sectors were overwritten
+ * - cached => free: cached sectors were overwritten
+ *
+ * - metadata => free: metadata was freed
+ *
+ * Oddities:
+ * - cached => dirty: a device was removed so formerly replicated data
+ *                    is no longer sufficiently replicated
+ * - free => cached: cannot happen
+ * - free => dirty: cannot happen
+ * - free => metadata: cannot happen
+ */
+
+#include "bcachefs.h"
+#include "buckets.h"
+
+#include <trace/events/bcachefs.h>
+
+#define bucket_cmpxchg(g, old, new, expr)			\
+do {								\
+	old = (g)->mark;					\
+	while (1) {						\
+		u32 _v;						\
+								\
+		new.counter = old.counter;			\
+		expr;						\
+		_v = cmpxchg(&(g)->mark.counter,		\
+			     old.counter,			\
+			     new.counter);			\
+		if (old.counter == _v)				\
+			break;					\
+		old.counter = _v;				\
+	}							\
+} while (0)
+
+void bch_mark_free_bucket(struct cache *ca, struct bucket *g)
+{
+	struct bucket_mark old, new;
+
+	bucket_cmpxchg(g, old, new, ({
+		BUG_ON(old.dirty_sectors);
+		BUG_ON(old.cached_sectors);
+		new.counter = 0;
+	}));
+}
+
+void bch_mark_alloc_bucket(struct cache *ca, struct bucket *g)
+{
+	struct bucket_mark old, new;
+
+	bucket_cmpxchg(g, old, new, ({
+		BUG_ON(old.dirty_sectors);
+		new.counter = 0;
+		new.owned_by_allocator = 1;
+	}));
+
+	if (!old.owned_by_allocator && old.cached_sectors)
+		trace_bcache_invalidate(ca, g - ca->buckets,
+					old.cached_sectors);
+}
+
+void bch_mark_metadata_bucket(struct cache *ca, struct bucket *g)
+{
+	struct bucket_mark old, new;
+
+	bucket_cmpxchg(g, old, new, ({
+		BUG_ON(old.cached_sectors);
+		BUG_ON(old.dirty_sectors);
+		new.is_metadata = 1;
+		new.owned_by_allocator = 0;
+	}));
+}
+
+#define saturated_add(ca, dst, src, max)			\
+do {								\
+	if ((dst) == (max))					\
+		;						\
+	else if ((dst) + (src) <= (max))			\
+		dst += (src);					\
+	else {							\
+		dst = (max);					\
+		trace_bcache_sectors_saturated(ca);		\
+	}							\
+} while (0)
+
+void bch_mark_data_bucket(struct cache *ca, struct bucket *g,
+			  int sectors, bool dirty)
+{
+	struct bucket_mark old, new;
+
+	bucket_cmpxchg(g, old, new, ({
+		BUG_ON(old.is_metadata);
+		if (dirty)
+			saturated_add(ca, new.dirty_sectors, sectors,
+				      GC_MAX_SECTORS_USED);
+		else
+			saturated_add(ca, new.cached_sectors, sectors,
+				      GC_MAX_SECTORS_USED);
+	}));
+}

fs/bcachefs/buckets.h
@@ -1,0 +1,32 @@
+/*
+ * Code for manipulating bucket marks for garbage collection.
+ *
+ * Copyright 2014 Datera, Inc.
+ */
+
+#ifndef _BUCKETS_H
+#define _BUCKETS_H
+
+#include "bcachefs.h"
+
+/* The dirty and cached sector counts saturate. If this occurs,
+ * reference counting alone will not free the bucket, and a btree
+ * GC must be performed. */
+#define GC_MAX_SECTORS_USED ((1U << 15) - 1)
+
+static inline bool bucket_unused(struct bucket *b)
+{
+	return !b->mark.counter;
+}
+
+static inline unsigned bucket_sectors_used(struct bucket *b)
+{
+	return b->mark.dirty_sectors + b->mark.cached_sectors;
+}
+
+void bch_mark_free_bucket(struct cache *, struct bucket *);
+void bch_mark_alloc_bucket(struct cache *, struct bucket *);
+void bch_mark_metadata_bucket(struct cache *, struct bucket *);
+void bch_mark_data_bucket(struct cache *, struct bucket *, int, bool);
+
+#endif